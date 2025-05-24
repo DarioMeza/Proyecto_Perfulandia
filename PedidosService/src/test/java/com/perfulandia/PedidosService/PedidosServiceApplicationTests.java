9162ff1a173cb3821e7ee9b97b01d3ff1046fee4
Darío Meza <dari.meza@duocuc.cl>
2025-05-23 22:24:30 -0400

    Estructura inicial microservicios

---
+++ PedidosService/src/test/java/com/perfulandia/PedidosService/PedidosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.PedidosService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ UsuariosService/src/test/java/com/perfulandia/UsuariosService/UsuariosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.UsuariosService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
